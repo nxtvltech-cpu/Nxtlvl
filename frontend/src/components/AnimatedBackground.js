9ea80f73b9c8c8dab381b22027cdb54684237642
NXTLVL Developer <dev@nxtlvl.gg>
2025-10-09 03:45:33 +0000

    auto-commit for 627b1e16-bda2-4015-9e97-ac079911abf7

---
+++ frontend/src/components/AnimatedBackground.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const AnimatedBackground = () => {
+  return (
+    <div className="global-animated-background">
+      {/* Animated Background Grid */}
+      <div className="global-grid"></div>
+      
+      {/* Scanning Light Effects */}
+      <div className="global-scan-line"></div>
+      <div className="global-scan-line-2"></div>
+      
+      {/* Light Beams */}
+      <div className="global-light-beam global-light-beam-1"></div>
+      <div className="global-light-beam global-light-beam-2"></div>
+      
+      {/* Moving Particles */}
+      <div className="global-particles">
+        <div className="global-particle"></div>
+        <div className="global-particle"></div>
+        <div className="global-particle"></div>
+        <div className="global-particle"></div>
+        <div className="global-particle"></div>
+        <div className="global-particle"></div>
+        <div className="global-particle"></div>
+        <div className="global-particle"></div>
+        <div className="global-particle"></div>
+        <div className="global-particle"></div>
+        <div className="global-particle"></div>
+        <div className="global-particle"></div>
+      </div>
+      
+      {/* Light Orbs */}
+      <div className="global-light-orb global-light-orb-1"></div>
+      <div className="global-light-orb global-light-orb-2"></div>
+      <div className="global-light-orb global-light-orb-3"></div>
+      <div className="global-light-orb global-light-orb-4"></div>
+      
+      {/* Energy Dots */}
+      <div className="global-energy-dot"></div>
+      <div className="global-energy-dot"></div>
+      <div className="global-energy-dot"></div>
+      <div className="global-energy-dot"></div>
+      <div className="global-energy-dot"></div>
+      <div className="global-energy-dot"></div>
+      
+      {/* Circuit Lines */}
+      <div className="global-circuit-line global-circuit-line-1"></div>
+      <div className="global-circuit-line global-circuit-line-2"></div>
+      <div className="global-circuit-line global-circuit-line-3"></div>
+      <div className="global-circuit-line global-circuit-line-4"></div>
+    </div>
+  );
+};
+
+export default AnimatedBackground;
